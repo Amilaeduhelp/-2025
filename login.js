b684b2ffb31281cb9819722441a244f76853a2ac
Amilaeduhelp <amila.udavatta@gmail.com>
2025-10-14 11:46:00 +0530

    Update login.js

--- login.js
+++ login.js
@@ -15,6 +15,7 @@ var credentials = {
   "RUKSHI": { hash: "9d980933f27f5763decdc1879ade71901aca981c1467feb55d1aaf0b4b4c4f5b", created: "2025-10-10" },
   "USER001": { hash: "8d969eef6ecad3c29a3a629280e686cf0c3f5d5a86aff3ca12020c923adc6c92", created: "2025-01-01" },
   "USER002": { hash: "481f6cc0511143ccdd7e2d1b1b94faf0a700a8b49cd13922a70b5ae28acaa8c5", created: "2025-01-15" },
+  "NAVODI": { hash: "1d2b158b3ce0c3251f7398b9516db4e60c6fa3b2037179bfe45239688a856261", created: "2025-10-14" },
   "USER003": { hash: "5906ac361a137e2d286465cd6588ebb5ac3f5d5a86aff3ca12020c923adc6c92", created: "2025-02-01" }
 };
 
